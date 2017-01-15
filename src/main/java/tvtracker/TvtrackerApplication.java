8a00e1c2d0736ca5da28ca57805ef93c8e31d56e
Tore Skog <toreskog@live.com>
2017-01-15 15:15:34 +0100

    Initial commit

---
+++ src/main/java/tvtracker/TvtrackerApplication.java
@@ -0,0 +1,12 @@
+package tvtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TvtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TvtrackerApplication.class, args);
+	}
+}
