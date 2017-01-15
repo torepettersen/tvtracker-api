3140b583e2bd66e92766d795ba478b85f9c51c1b
Tore Skog <toreskog@live.com>
2017-01-15 16:13:46 +0100

    Added in memory security

---
+++ src/main/java/tvtracker/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package tvtracker.config;
+
+import java.security.SecureRandom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	//@Autowired
+    //private UserSecurityService userSecurityService;
+	
+	/** The encryption SALT. */
+    private static final String SALT = "hr54jg84qhdnvvp941";
+    
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12, new SecureRandom(SALT.getBytes()));
+    }
+
+    private static final String[] PUBLIC_MATCHERS = {
+		"/"
+    };
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    	http
+			.csrf().disable()
+			.sessionManagement()
+	        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http
+			.httpBasic();
+		http
+			.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHERS).permitAll()
+			.anyRequest().authenticated();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                //.userDetailsService(userSecurityService)
+                //.passwordEncoder(passwordEncoder());
+    	
+    		.inMemoryAuthentication()
+    		.withUser("user").password("password").roles("USER");
+    }
+    
+}
